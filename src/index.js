1991ac9baa5cbab2374d0769adfdbf6d7a1c7975
pkieber <113725311+pkieber@users.noreply.github.com>
2023-07-12 14:43:53 +0200

    add product component and routing

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
-import React from "react"
-import ReactDom from "react-dom"
-import Navbar from "./components/navbar";
+import React from "react";
+import ReactDom from "react-dom";
 import "bootstrap/dist/css/bootstrap.min.css";
+import "./index.css";
+import App from "./App";
 
-ReactDom.render(<Navbar/>, document.getElementById('root'));
-
+ReactDom.render(<App></App>, document.getElementById('root'));
