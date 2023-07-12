d34633ae44619a3c47317668254c9b3531c29eb9
pkieber <113725311+pkieber@users.noreply.github.com>
2023-07-12 20:14:51 +0200

    add logo + search container

--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -13,10 +13,17 @@ class Navbar extends Component {
      * @returns {JSX.Element} JSX representing the component's UI.
      */
     render() { 
-        return <nav className="navbar bg-light">
-            <div className="container-fluid">
-                <a className="navbar-brand" href="/">Navbar</a>
+        return <nav class="nav-bar-container p-3 mb-2 bg-primary text-white">
+            <div className="logo-container container-fluid">
+                <a className="navbar-brand" href="/">
+                    <img src="/assets/img/logo.png" width="25" height="25" class="d-inline-block align-top" alt=""/>
+                    Veggie Shop
+                </a>
             </div>
+            <form className="search-container form-inline my-2 my-lg-0">
+                <input className="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search"/>
+                <button className="btn btn-primary" type="submit">Search</button>
+            </form>
         </nav>
     }
 }
--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from "react";
 import { createRoot } from 'react-dom/client';
 import "bootstrap/dist/css/bootstrap.min.css";
-import "./index.css";
+import "./style.css";
 import App from "./App";
 
 const root = createRoot(document.getElementById('root'));
