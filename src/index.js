834f3886416e5eecd6de0fbcf6d70cfb1a4b25b8
pkieber <113725311+pkieber@users.noreply.github.com>
2023-07-12 13:54:06 +0200

    bootstrap importieren

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import React from "react"
 import ReactDom from "react-dom"
 import Navbar from "./components/navbar";
+import "bootstrap/dist/css/bootstrap.min.css";
 
 ReactDom.render(<Navbar/>, document.getElementById('root'));
 
