b74916807b6c4e64e61ada623f5b1103a50169a7
pkieber <113725311+pkieber@users.noreply.github.com>
2023-07-12 13:27:07 +0200

    add navbar component

--- src/index.js
+++ src/index.js
@@ -1,7 +1,6 @@
 import React from "react"
 import ReactDom from "react-dom"
+import Navbar from "./components/navbar";
 
-const element = <h1>Hello World</h1>;
-
-ReactDom.render(element, document.getElementById('root'));
+ReactDom.render(<Navbar/>, document.getElementById('root'));
 
